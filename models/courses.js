ced3d4f714da2a04bedc16935ccc349d1df777d0
joshowen04 <owe21015@byui.edu>
2023-01-21 23:22:22 -0300

    submitting

--- models/courses.js
+++ models/courses.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 const CoursesSchema = new Schema({
   id: {
     type: int,
--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -1,24 +1,22 @@
-  'use strict';
-  module.exports = function(app) {
-    const contacts = require('../controllers/contactsController');
-  
-    app.route('/contacts')
-      .get(contacts.list_all_contacts)
-      .post(contacts.create_a_contact);
-    
-    app.route('/contacts/:contactId')
-      .get(contacts.read_a_contact)
-      .put(contacts.update_a_contact)
-      .delete(contacts.delete_a_contact)
-      ;
-  };
+'use strict';
+module.exports = function (app) {
+  const contacts = require('../controllers/contactsController');
 
-  // const routes = require('express').Router();
-  // const contacts = require('../controllers/contactsController');
-  
-  // routes.get('/contacts', contacts.list_all_contacts);
-  // routes.post('/contacts', contacts.create_a_contact);
-  // routes.get('/contacts', contacts.read_a_contact);
-  // // routes.get('/contacts', contacts.list_all_contacts);
-  // // routes.get('/courses', contacts.displayCourses);
-  // module.exports = routes;
+  app.route('/contacts').get(contacts.list_all_contacts).post(contacts.create_a_contact);
+
+  app
+    .route('/contacts/:contactId')
+    .get(contacts.read_a_contact)
+    .put(contacts.update_a_contact)
+    .delete(contacts.delete_a_contact);
+};
+
+// const routes = require('express').Router();
+// const contacts = require('../controllers/contactsController');
+
+// routes.get('/contacts', contacts.list_all_contacts);
+// routes.post('/contacts', contacts.create_a_contact);
+// routes.get('/contacts', contacts.read_a_contact);
+// // routes.get('/contacts', contacts.list_all_contacts);
+// // routes.get('/courses', contacts.displayCourses);
+// module.exports = routes;
