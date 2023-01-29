bc6b5308f703df0fae58ff0555e07390dd110004
joshowen04 <owe21015@byui.edu>
2023-01-28 23:44:43 -0600

    swagger

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -1,9 +1,12 @@
 'use strict';
 module.exports = function (app) {
   const contacts = require('../controllers/contactsController');
+  const swaggerUi = require('swagger-ui-express');
+  const swaggerDocument = require('../swagger.json');
+  app.use(swaggerUi.serve);
 
   app.route('/contacts').get(contacts.list_all_contacts).post(contacts.create_a_contact);
-
+  app.route('/api-docs').get(swaggerUi.setup(swaggerDocument));
   app
     .route('/contacts/:contactId')
     .get(contacts.read_a_contact)
